3a17093e7f3007bbd749fa931582b71789866a71
Boem22 <>
2025-03-17 21:10:29 +0100

    a

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-// Use Render's provided PORT or default to 8080
 const PORT = process.env.PORT || 8080;
 
-// Create an HTTP server
 const server = http.createServer((req, res) => {
-    // Serve the index.html file
     if (req.url === '/') {
         const filePath = path.join(__dirname, 'index.html');
         fs.readFile(filePath, (err, data) => {
@@ -27,40 +24,30 @@ const server = http.createServer((req, res) => {
     }
 });
 
-// Create a WebSocket server by passing the HTTP server
 const wss = new WebSocket.Server({ server });
 
-// Store all connected clients
 const clients = new Set();
 
 wss.on('connection', (ws) => {
     console.log('New client connected');
-
-    // Add the new client to the set of connected clients
     clients.add(ws);
 
-    // Handle incoming messages from clients
     ws.on('message', (message) => {
         console.log('Received:', message.toString());
 
-        // Broadcast the message to all connected clients
         clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
             }
         });
     });
 
-    // Handle client disconnection
     ws.on('close', () => {
         console.log('Client disconnected');
-        // Remove the client from the set of connected clients
         clients.delete(ws);
     });
 });
 
-// Start the server
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`WebSocket server is running on wss://myserver-1jxx.onrender.com`);
 });
