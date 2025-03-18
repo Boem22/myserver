4a47ab12c436408b0916854850e5959510e5cdd8
Boem22 <>
2025-03-18 18:34:06 +0100

    a

--- server.js
+++ server.js
@@ -6,53 +6,33 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 const DATA_FILE = 'data.json';
 
-// Persistent storage
-let messageHistory = [];
-let levelHistory = [];
-let levelVotes = {};
-let userVotes = {};
-
-function loadData() {
-  try {
-    const data = fs.readFileSync(DATA_FILE);
-    const parsed = JSON.parse(data);
-    messageHistory = parsed.messages || [];
-    levelHistory = parsed.levels || [];
-    levelVotes = parsed.levelVotes || {};
-    userVotes = parsed.userVotes || {};
-    console.log('Loaded persistent data');
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      fs.writeFileSync(DATA_FILE, JSON.stringify({ messages: [], levels: [], levelVotes: {}, userVotes: {} }));
-      console.log('Created new data file');
-    } else {
-      console.error('Error loading data:', err);
-    }
+// Load data from file
+let data = {};
+try {
+  data = JSON.parse(fs.readFileSync(DATA_FILE));
+} catch (err) {
+  if (err.code === 'ENOENT') {
+    fs.writeFileSync(DATA_FILE, JSON.stringify({ messages: [], levels: [], levelVotes: {}, userVotes: {} }));
+  } else {
+    console.error('Error loading data:', err);
   }
 }
 
+// Save data to file
 function saveData() {
-  fs.writeFileSync(DATA_FILE, JSON.stringify({
-    messages: messageHistory,
-    levels: levelHistory,
-    levelVotes: levelVotes,
-    userVotes: userVotes
-  }));
-  console.log('Data saved');
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data));
 }
 
-// Initial data load
-loadData();
-
+// Create HTTP server
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Error loading index.html');
       } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
+        res.end(content);
       }
     });
   } else {
@@ -61,6 +41,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
+// WebSocket server
 const wss = new WebSocket.Server({ server });
 const clients = new Set();
 
@@ -68,14 +49,16 @@ wss.on('connection', (ws) => {
   console.log('New client connected');
   clients.add(ws);
 
+  // Send initial data to the client
   ws.send(JSON.stringify({
     type: 'init',
-    messages: messageHistory,
-    levels: levelHistory,
-    levelVotes: levelVotes,
-    userVotes: userVotes
+    messages: data.messages,
+    levels: data.levels,
+    levelVotes: data.levelVotes,
+    userVotes: data.userVotes
   }));
 
+  // Handle incoming messages
   ws.on('message', (rawData) => {
     let message;
     try {
@@ -93,27 +76,23 @@ wss.on('connection', (ws) => {
 
     switch (message.type) {
       case 'comment':
-        message.id = message.id || Date.now();
-        message.timestamp = message.timestamp || Date.now();
-        message.upvotes = message.upvotes || 0;
-        message.downvotes = message.downvotes || 0;
-        messageHistory.push(message);
+        data.messages.push(message);
         break;
 
       case 'new_level':
-        if (!levelHistory.some(l => l.id === message.level.id)) {
-          levelHistory.push(message.level);
-          levelVotes[message.level.id] = { up: 0, down: 0 };
+        if (!data.levels.some(l => l.id === message.level.id)) {
+          data.levels.push(message.level);
+          data.levelVotes[message.level.id] = { up: 0, down: 0 };
         }
         break;
 
       case 'vote_level':
-        levelVotes[message.levelId] = levelVotes[message.levelId] || { up: 0, down: 0 };
-        levelVotes[message.levelId][message.value === 1 ? 'up' : 'down']++;
+        data.levelVotes[message.levelId] = data.levelVotes[message.levelId] || { up: 0, down: 0 };
+        data.levelVotes[message.levelId][message.value === 1 ? 'up' : 'down']++;
         break;
 
       case 'vote_comment':
-        const comment = messageHistory.find(m => m.id === message.messageId);
+        const comment = data.messages.find(m => m.id === message.messageId);
         if (comment) {
           if (message.value === 1) comment.upvotes++;
           else comment.downvotes++;
@@ -121,16 +100,18 @@ wss.on('connection', (ws) => {
         break;
 
       case 'delete_message':
-        messageHistory = messageHistory.filter(m => String(m.id) !== String(message.messageId));
+        data.messages = data.messages.filter(m => String(m.id) !== String(message.messageId));
         break;
 
       case 'delete_level':
-        levelHistory = levelHistory.filter(l => String(l.id) !== String(message.levelId));
-        delete levelVotes[message.levelId];
+        data.levels = data.levels.filter(l => String(l.id) !== String(message.levelId));
+        delete data.levelVotes[message.levelId];
         break;
     }
 
     saveData();
+
+    // Broadcast the message to all clients
     clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify(message));
@@ -138,12 +119,14 @@ wss.on('connection', (ws) => {
     });
   });
 
+  // Handle client disconnection
   ws.on('close', () => {
     clients.delete(ws);
     console.log('Client disconnected');
   });
 });
 
+// Start the server
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
